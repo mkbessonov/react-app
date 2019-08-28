ec0e7187096f31fb27a2067ac2f860e6f63faf26
mkbessonov <mkbessonov@mail.ru>
2019-08-28 19:57:24 +0300

    Добавлено взаимодействие с компонентом

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 class Square extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render(){
         return (
-            <button className = "square">
-            {this.props.value}
+            <button
+                className = "square"
+                onClick = {() => this.setState({value: 'X'})}
+            >
+            {this.state.value}
             </button>
         );
     }
